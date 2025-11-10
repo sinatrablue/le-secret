f7a39e311bda8c3ac103fe19426a3cb0bb8dfe85
Robin Simonot <robin.simonot@id6tm.org>
2025-11-10 19:02:04 +0100

    fix: new attempt for png

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import GlitchText from "@/components/GlitchText";
 import { Drawer } from "vaul";
-import QuestionMark from "/question-mark-icon.png";
 
 function App() {
   return (
     <div className="flex flex-col h-dvh">
       <Drawer.Root>
         <Drawer.Trigger>
-          <img src={QuestionMark} className="h-8 w-8 absolute top-2 right-2" />
+          <img
+            src="le-secret/question-mark-icon.png"
+            className="h-8 w-8 absolute top-2 right-2"
+          />
         </Drawer.Trigger>
         <Drawer.Portal>
           <Drawer.Overlay className="fixed inset-0 bg-black/40" />
