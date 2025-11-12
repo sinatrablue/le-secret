9df6a45f9fcb989c4b2e2e015a7f1ee0778b1b3b
Robin Simonot <48838914+sinatrablue@users.noreply.github.com>
2025-11-12 11:15:21 +0100

    feat: step2 with visitors identification and secrets revealed (#1)
    * feat: added custom visitor name based on param
    
    * feat: typing effect on name
    
    * feat: handled special content with animations
    
    * fix: coordinates were wrong
    
    ---------
    
    Co-authored-by: Robin Simonot <robin.simonot@id6tm.org>

---
+++ src/constants.ts
@@ -0,0 +1,54 @@
+export const VISITORS = [
+  "dodo",
+  "bobore",
+  "aela",
+  "laure",
+  "soso",
+  "alienor",
+  "lea",
+  "manue",
+  "assia",
+  "robin",
+] as const;
+
+export const NAMES = [
+  "Dodo de la tisane à Dodo",
+  "Hellébore",
+  "Aéla",
+  "La folle de SMLT",
+  "Sonia",
+  "Aliénor",
+  "Léa",
+  "Joggo",
+  "Jessie",
+  "James",
+] as const;
+
+const APOLOGY =
+  "Tu ne détiens pas d'information cruciale à partager avec les autres, désolés. ";
+
+export const CONTENT_FOR_LAURE =
+  APOLOGY +
+  "Cela n'empêche pas qu'on t'aime très fort et qu'on a hâte d'autres soirées tisanes trépidantes (la tisane à Dodo, bien sûr).";
+
+export const CONTENT_FOR_SONIA =
+  APOLOGY +
+  "Pour autant, nous t'aimons un peu (ça passe) et te souhaitons toute la réussite dans ta reconversion.";
+
+export const CONTENT_FOR_ALIENOR =
+  APOLOGY +
+  "On se recontacte par contre au plus vite pour la Daronnade. On t'aime beaucoup fort.";
+
+export const CONTENT_FOR_LEA =
+  APOLOGY +
+  "Merci de t'impliquer autant et de te creuser la tête avec nos indices douteux. Tu es la meilleure et on te kiffe. *CheckDeNigga*";
+
+export const CONTENT_FOR_MANUE =
+  APOLOGY +
+  "Nonobstant, on te kiffe de ouf et tu es une grande soeur qu'on aimerai tous avoir.";
+
+export const CONTENT_FOR_ASSIA =
+  "Afin de protéger le monde de la dévastation...";
+
+export const CONTENT_FOR_ROBIN =
+  "... Afin de ralier tous les peuples à notre nation.";
