79650f41d1f326e28818d542a38c9c9d22b34f99
Robin Simonot <robin.simonot@id6tm.org>
2025-11-13 12:24:29 +0100

    feat: step1

--- src/components/FinalQuizz.tsx
+++ src/components/FinalQuizz.tsx
@@ -1,19 +1,26 @@
 import Stepper, { Step } from "@/components/Stepper";
-import type { FC } from "react";
+import YearMetForm from "@/components/YearMetForm";
+import { useState, type FC } from "react";
 
 type FinalQuizzProps = {
   setIsQuizzCompleted(complete: boolean): void;
 };
 
 export const FinalQuizz: FC<FinalQuizzProps> = ({ setIsQuizzCompleted }) => {
+  const [isNextDisabled, setIsNextDisabled] = useState(true);
   return (
     <Stepper
-      className="min-h-full"
+      className="min-h-fit"
+      contentClassName="min-h-[200px]"
       backButtonText="Précédent"
       nextButtonText="Suivant"
       onFinalStepCompleted={() => setIsQuizzCompleted(true)}
+      nextButtonProps={{ disabled: isNextDisabled }}
+      onStepChange={() => setIsNextDisabled(true)}
     >
-      <Step>step 1</Step>
+      <Step>
+        <YearMetForm setIsNextDisabled={setIsNextDisabled} />
+      </Step>
       <Step>step2</Step>
     </Stepper>
   );
